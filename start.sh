6ac750eccf3be995767803b56caedb97d917ba25
Tomoya Mikami <trymorecc@gmail.com>
2018-06-06 13:47:48 +0900

    networkの修正

--- start.sh
+++ start.sh
@@ -3,8 +3,9 @@ export RAILS_ENV=production
 kill `cat tmp/pids/server.pid`
 #git pull origin master
 bundle install --without test development
+export SECRET_KEY_BASE=bundle exec rake secret
 bundle exec rails db:migrate RAILS_ENV=production
 bundle exec rails assets:precompile RAILS_ENV=production
-export SECRET_KEY_BASE=`bundle exec rake secret`
+export SECRET_KEY_BASE=bundle exec rake secret
 export RAILS_ENV=production
 bundle exec rails s -d -e production -p 3000 -b 0.0.0.0
